2550060d562f516e11ce7f25d365adf52da4a92a
SonarJayesh <jayeshsonar2824@gmail.com>
2026-04-09 22:01:07 +0530

    [ADDED]: Jayesh Java Collection framework code for Simple LinkedList Program for Add Student name

---
+++ LinkedList_Demo1.java
@@ -0,0 +1,15 @@
+package Collection_Framework;
+import java.util.LinkedList;
+
+public class LinkedList_Demo1 {
+    public static void main(String[] args) {
+        LinkedList<String> studName = new LinkedList<String>();
+
+        studName.add("Umesh");
+        studName.add("Jayesh");
+        studName.add("Shubham");
+        studName.add("Ritesh");
+
+        System.out.println(studName);
+    }    
+}
